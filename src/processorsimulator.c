99e9cbb7e37d5272ed9e6d20d622fcbbeab9eda7
Ribamar Junior <92236780+RbmrJnr@users.noreply.github.com>
2025-02-14 18:28:05 -0300

    Implementação da função ZerarStruct

--- src/processorsimulator.c
+++ src/processorsimulator.c
@@ -6,14 +6,22 @@ typedef struct{
     uint16_t PC;
     uint16_t IR;
     uint16_t SP;
-    uint16_t FLAGS;
 
     uint16_t Memoria[1024];
     uint16_t Pilha[256];
+
+    struct{
+        uint16_t Zero;
+        uint16_t Negative;
+        uint16_t Carry;
+        uint16_t Overflow;
+    } FLAGS;
+    
 } processorsimulator;
 
-void readArchive(const char *Arquivo) {
-    char endereco[50], conteudo[50];
+void readArchive(processorsimulator *processador, const char *Arquivo) {
+    uint16_t endereco, conteudo;
+    char linha[50];
     FILE *arq;
 
     arq = fopen(Arquivo, "rt");
@@ -22,17 +30,46 @@ void readArchive(const char *Arquivo) {
         return;
     }
     printf("\nConteúdo do arquivo:\n");
-    while(fscanf(arq, "%30[^:]: %30[^\n]\n", endereco, conteudo) == 2){
-        printf("Endereco: %s | Conteudo: %s\n", endereco, conteudo);
+    while (fgets(linha, sizeof(linha), arq)){
+        if(sscanf(linha, "%hx: %hx", &endereco, &conteudo) == 2){
+            if (endereco < 1024) {
+                processador->Memoria[endereco / 2] = conteudo;
+                printf("Endereco: 0x%04X | Conteudo: 0x%04X\n", endereco, conteudo);
+            }else{
+                printf("Erro: Endereco 0x%04X fora dos limites!\n", endereco);
+            }
+        }else{
+            printf("Erro na linha: %s", linha);
+        }
     }
-
     fclose(arq);
 }
 
 void executeInstruction(processorsimulator *processador){
+    
+}
 
+void zerarStruct(processorsimulator *processador){
+    for(int i = 0; i < 1024; i++){
+        processador->Memoria[i] = 0;
+    }
+    for(int i = 0; i < 256; i++){
+        processador->Pilha[i] = 0;
+    }
+    for(int i = 0; i < 8; i++){
+        processador->Registradores[i] = 0;
+    }
+    processador->IR = 0;
+    processador->SP = 0x8200;
+    processador->PC = 0;
+    processador->FLAGS.Carry = 0;
+    processador->FLAGS.Negative = 0;
+    processador->FLAGS.Overflow = 0;
+    processador->FLAGS.Zero = 0;
 }
 
 int main(){
-    readArchive("../arquivo.txt");
+    processorsimulator processador;
+    zerarStruct(&processador);
+    readArchive(&processador,"../arquivo.txt");
 }
